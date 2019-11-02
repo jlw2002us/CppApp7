78a5fcf4eb417a631ba499cf4209b8e85126c644
jlw2002us <30751177+jlw2002us@users.noreply.github.com>
2019-11-02 15:35:54 -0500

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jenniferwasson
+ *
+ * Created on November 2, 2019, 3:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
